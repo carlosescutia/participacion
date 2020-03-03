fba567eadec26a9acb8d79186c2198c4e6a60b3a
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-03-03 15:35:22 -0600

    Se agrega menu lateral

--- application/db/crear_bd.sql
+++ application/db/crear_bd.sql
@@ -7,3 +7,45 @@ CREATE TABLE usuarios (
     dependencia text
 );
 
+DROP TABLE IF EXISTS actores;
+CREATE TABLE actores (
+    cve_actor integer, 
+    apellido_pa text,
+    apellido_ma text,
+    nombre text,
+    fecha_nacimiento date,
+    sexo text,
+    calle text,
+    num_exterior text,
+    num_interior text,
+    colonia text,
+    codigo_postal integer,
+    ciudad text,
+    estado text,
+    externo_interno text,
+    ine text,
+    ambito text,
+    sector text,
+    organización text,
+    telefono_fijo text,
+    telefono_celular text,
+    correo_personal text,
+    correo_laboral text,
+    asistente text,
+    correo_asistente text,
+    telefono_asistente text,
+    historial text,
+    ambito_organismo text,
+    ultimo_periodo text,
+    consejo_actual text,
+    cargo text,
+    permiso text,
+    otros_espacios text,
+    experiencia_exitosa text,
+    fecha_experiencia_exitosa date,
+    desea_colaborar text,
+    profesion text,
+    perfil text
+);
+
+
