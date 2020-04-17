310e414173b655bb07fd27d4b02ac7f0dd2b4fe9
Carlos Escutia <carlosescutia@gmail.com>
2020-04-17 13:21:30 -0500

    Se agrega imagen a seccion inicio.

--- application/views/inicio/inicio.php
+++ application/views/inicio/inicio.php
@@ -2,21 +2,30 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Inicio</h1>
     </div>
-    <div class="col-md-12">
-        <?php include "bienvenida.php"; ?>
-    </div>
-
-    <hr />
-
-    <div class="col-md-12">
-        <div class="row">
-            <div class="col-md-4">
-                <?php include "datos_actores.php"; ?>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="col-md-12">
+                <?php include "bienvenida.php"; ?>
             </div>
-            <div class="col-md-4">
-                <?php include "datos_consejos.php"; ?>
+
+            <hr />
+
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <?php include "datos_actores.php"; ?>
+                    </div>
+                    <div class="col-md-6">
+                        <?php include "datos_consejos.php"; ?>
+                    </div>
+                </div>
             </div>
         </div>
+
+        <div class="col-md-3 ml-5">
+            <img src="<?=base_url();?>img/participacion_social_inicio.jpg" class="img-fluid rounded">
+        </div>
+
     </div>
 
     <hr />
