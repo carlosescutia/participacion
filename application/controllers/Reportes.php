aa80ae6c2acad688b69bdbd8a4d984290a266c76
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-04-28 17:53:15 -0500

    Se completa reporte de listado de Actores.

--- application/controllers/Reportes.php
+++ application/controllers/Reportes.php
@@ -7,6 +7,7 @@ class Reportes extends CI_Controller {
         $this->load->helper('url');
         $this->load->model('usuarios_model');
         $this->load->model('actores_model');
+        $this->load->model('sectores_model');
 
     }
 
@@ -25,8 +26,46 @@ class Reportes extends CI_Controller {
         }
     }
 
+    public function reporte_actores_01()
+    {
+        if ($this->session->userdata('logueado')) {
+            $data['usuario_clave'] = $this->session->userdata('clave');
+            $data['usuario_nombre'] = $this->session->userdata('nombre');
+            $dependencia = $this->session->userdata('dependencia');
+            $data['usuario_dependencia'] = $dependencia;
+
+            $data['actores'] = $this->actores_model->get_reporte_actores_01($dependencia);
+            $data['sectores'] = $this->sectores_model->get_sectores();
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('reportes/reporte_actores_01', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+
+    public function reporte_actores_01_csv()
+    {
+        if ($this->session->userdata('logueado')) {
+            $data['usuario_clave'] = $this->session->userdata('clave');
+            $data['usuario_nombre'] = $this->session->userdata('nombre');
+            $dependencia = $this->session->userdata('dependencia');
+            $data['usuario_dependencia'] = $dependencia;
+
+            $this->load->dbutil();
+            $this->load->helper('file');
+            $this->load->helper('download');
+
+			$sql = "select a.nombre, a.apellido_pa, a.apellido_ma, m.nom_mun, a.sexo, s.nom_sector, (select string_agg(c.nom_consejo, ',') as consejos from consejos_actores ca left join consejos c on ca.cve_consejo = c.cve_consejo where ca.cve_actor = a.cve_actor), t.nom_tipo from actores a left join municipios m on a.cve_mun = m.cve_mun left join sectores s on a.cve_sector = s.cve_sector left join tipos t on a.cve_tipo = t.cve_tipo where a.dependencia=? order by a.nombre";
+			$query = $this->db->query($sql, array($dependencia));
+
+            $delimiter = ",";
+            $newline = "\r\n";
+            $data = $this->dbutil->csv_from_result($query, $delimiter, $newline);
+            force_download("reporte_actores_01.csv", $data);
+        }
+    }
 
 }
-
-
-
