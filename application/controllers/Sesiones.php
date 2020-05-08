a7067160f9937da68a18c12ffcfcb2cb74e92bae
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-05-08 14:56:57 -0500

    Se completa migración de validación de campos en sección Sesiones; se completa evitar limpiar información en captura de campos en sección Sesiones.

--- application/controllers/Sesiones.php
+++ application/controllers/Sesiones.php
@@ -5,11 +5,14 @@ class Sesiones extends CI_Controller {
     {
         parent::__construct();
         $this->load->helper('url');
+        $this->load->helper('form');
         $this->load->model('sesiones_model');
+        $this->load->library('form_validation');
+        $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
 
     }
 
-    public function detalle($cve_sesion)
+    public function detalle($cve_sesion, $cve_consejo)
     {
         if ($this->session->userdata('logueado')) {
             $data['error_sesion'] = $this->session->flashdata('error_sesion');
@@ -18,7 +21,7 @@ class Sesiones extends CI_Controller {
             $dependencia = $this->session->userdata('dependencia');
             $data['usuario_dependencia'] = $dependencia;
 
-            $data['sesiones'] = $this->sesiones_model->get_sesion($cve_sesion);
+            $data['sesiones'] = $this->sesiones_model->get_sesion_consejo($cve_sesion, $cve_consejo);
 
             $this->load->view('templates/header', $data);
             $this->load->view('sesiones/detalle', $data);
@@ -28,9 +31,45 @@ class Sesiones extends CI_Controller {
         }
     }
 
-    public function nuevo($cve_consejo)
+    public function guardar($cve_consejo)
     {
         if ($this->session->userdata('logueado')) {
+
+            $this->form_validation->set_rules('nom_sesion','nombre','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('tipo','tipo','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('lugar','lugar','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('fecha','fecha','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('hora','hora','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('objetivo','objetivo','required',array('required' => '* requerido',));
+            $this->form_validation->set_rules('orden_dia','orden_dia','required',array('required' => '* requerido',));
+
+            if ($this->form_validation->run())
+            {
+                $sesion = $this->input->post();
+                $data = array(
+                    'cve_consejo' => $sesion['cve_consejo'],
+                    'nom_sesion' => $sesion['nom_sesion'],
+                    'tipo' => $sesion['tipo'],
+                    'lugar' => $sesion['lugar'],
+                    'fecha' => $sesion['fecha'],
+                    'hora' => $sesion['hora'],
+                    'objetivo' => $sesion['objetivo'],
+                    'orden_dia' => $sesion['orden_dia']
+                );
+                $cve_sesion = $sesion['cve_sesion'];
+                $this->sesiones_model->guardar($data, $cve_sesion);
+                redirect('consejos/lista');
+            } else {
+                if (validation_errors())
+                {
+                    $sesion = $this->input->post();
+                }
+            }
+
+            $data = array(
+                'sesiones' => $sesion
+            );
+
             $data['error_sesion'] = $this->session->flashdata('error_sesion');
             $data['usuario_nombre'] = $this->session->userdata('nombre');
             $dependencia = $this->session->userdata('dependencia');
@@ -38,40 +77,18 @@ class Sesiones extends CI_Controller {
 
             $data['cve_consejo'] = $cve_consejo;
 
-            $this->load->view('templates/header', $data);
-            $this->load->view('sesiones/nuevo', $data);
-            $this->load->view('templates/footer');
+            if (isset($data['sesiones']['cve_sesion']))
+            {
+                $this->load->view('templates/header', $data);
+                $this->load->view('sesiones/detalle', $data);
+                $this->load->view('templates/footer');
+            } else {
+                $this->load->view('templates/header', $data);
+                $this->load->view('sesiones/nuevo', $data);
+                $this->load->view('templates/footer');
+            }
         } else {
             redirect('inicio/iniciar_sesion');
-        }
-    }
-
-    public function guardar($cve_sesion=null)
-    {
-        if ($this->session->userdata('logueado')) {
-            $data['usuario_nombre'] = $this->session->userdata('nombre');
-            $dependencia = $this->session->userdata('dependencia');
-            $data['usuario_dependencia'] = $dependencia;
-
-            $sesion = $this->input->post();
-            if ($sesion) {
-                $cve_consejo = empty($sesion['cve_consejo']) ? null : $sesion['cve_consejo'];
-                $nom_sesion = empty($sesion['nom_sesion']) ? null : $sesion['nom_sesion'];
-                $tipo = empty($sesion['tipo']) ? null : $sesion['tipo'];
-                $lugar = empty($sesion['lugar']) ? null : $sesion['lugar'];
-                $fecha = empty($sesion['fecha']) ? null : $sesion['fecha'];
-                $hora = empty($sesion['hora']) ? null : $sesion['hora'];
-                $objetivo = empty($sesion['objetivo']) ? null : $sesion['objetivo'];
-                $orden_dia = empty($sesion['orden_dia']) ? null : $sesion['orden_dia'];
-
-                if ($cve_consejo && $nom_sesion && $tipo && $lugar && $fecha && $hora && $objetivo && $orden_dia) {
-                    $this->sesiones_model->guardar($cve_consejo, $nom_sesion, $tipo, $lugar, $fecha, $hora, $objetivo, $orden_dia, $cve_sesion);
-                } else {
-                    $this->session->set_flashdata('error_sesion', 'Capture todos los datos');
-                    redirect($_SERVER['HTTP_REFERER']);
-                }
-            }
-            redirect('consejos/lista');
         }
     }
 
--- application/views/sesiones/detalle.php
+++ application/views/sesiones/detalle.php
@@ -1,5 +1,6 @@
 <main role="main" class="ml-sm-auto px-4">
-    <form method="post" action="<?= base_url() ?>sesiones/guardar/<?=$sesiones['cve_sesion']?>">
+
+    <form method="post" action="<?= base_url() ?>sesiones/guardar/<?= $sesiones['cve_consejo'] ?>">
         <div class="col-md-12 mb-3 pb-2 pt-3 border-bottom">
             <div class="row">
                 <div class="col-md-4">
@@ -22,12 +23,14 @@
                     <div class="form-row">
                         <div class="form-group col-md-12">
                             <label for="nom_sesion">Nombre</label>
+                            <?php echo form_error('nom_sesion'); ?>
                             <input type="text" class="form-control" name="nom_sesion" id="nom_sesion" value="<?=$sesiones['nom_sesion'] ?>">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-4">
                             <label for="tipo">Tipo</label>
+                            <?php echo form_error('tipo'); ?>
                             <select class="custom-select" name="tipo" id="tipo">
                                 <option value=""></option>
                                 <option value="o" <?= ($sesiones['tipo'] == 'o') ? 'selected' : '' ?> >Ordinaria</option>
@@ -36,29 +39,35 @@
                         </div>
                         <div class="form-group col-md-4">
                             <label for="lugar">Lugar</label>
+                            <?php echo form_error('lugar'); ?>
                             <input type="text" class="form-control" name="lugar" id="lugar" value="<?=$sesiones['lugar'] ?>">
                         </div>
                         <div class="form-group col-md-2">
                             <label for="fecha">Fecha</label>
+                            <?php echo form_error('fecha'); ?>
                             <input type="date" class="form-control" name="fecha" id="fecha" value="<?=$sesiones['fecha'] ?>">
                         </div>
                         <div class="form-group col-md-2">
                             <label for="hora">Hora</label>
+                            <?php echo form_error('hora'); ?>
                             <input type="time" class="form-control" name="hora" id="hora" value="<?=$sesiones['hora'] ?>">
                         </div>
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="objetivo">Objetivo</label>
+                            <?php echo form_error('objetivo'); ?>
                             <textarea rows="6" class="form-control" name="objetivo" id="objetivo"><?=$sesiones['objetivo'] ?></textarea>
                         </div>
                         <div class="form-group col-md-6">
                             <label for="orden_dia">Orden del día</label>
+                            <?php echo form_error('orden_dia'); ?>
                             <textarea rows="6" class="form-control" name="orden_dia" id="orden_dia"><?=$sesiones['orden_dia'] ?></textarea>
                         </div>
                     </div>
                 </div>
                 <input type="hidden" name="cve_consejo" id="cve_consejo" value="<?=$sesiones['cve_consejo'] ?>">
+                <input type="hidden" name="cve_sesion" id="cve_sesion" value="<?=$sesiones['cve_sesion'] ?>">
             </form>
 
             <div class="col-md-3">
