18cb91659cc030410047bcdcd5ed0609912745f3
Carlos Escutia <carlosescutia@gmail.com>
2020-05-08 14:58:18 -0500

    Se renombra la sección Tablero a Inicio.

--- application/views/inicio/inicio.php
+++ application/views/inicio/inicio.php
@@ -1,6 +1,6 @@
 <main role="main" class="ml-sm-auto px-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h2>Tablero</h2>
+        <h2>Inicio</h2>
     </div>
     <div class="row">
         <div class="col-md-8">
