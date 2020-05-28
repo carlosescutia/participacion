ea3abd534d39c43dbc1baa03677e81e8bceb74e7
Carlos Escutia <carlosescutia@gmail.com>
2020-05-28 15:54:46 -0500

    Se eliminan los botones de los catálogos de Entidades y Municipios.

--- application/views/catalogos/lista.php
+++ application/views/catalogos/lista.php
@@ -13,38 +13,30 @@
             <?php include "ejes/boton.php" ?>
         </div>
         <div class="col-md-3">
-            <?php include "entidades/boton.php" ?>
+            <?php include "perfiles/boton.php" ?>
         </div>
     </div>
     <div class="row mb-3">
-        <div class="col-md-3">
-            <?php include "municipios/boton.php" ?>
-        </div>
-        <div class="col-md-3">
-            <?php include "perfiles/boton.php" ?>
-        </div>
         <div class="col-md-3">
             <?php include "roles/boton.php" ?>
         </div>
         <div class="col-md-3">
             <?php include "sectores/boton.php" ?>
         </div>
-    </div>
-    <div class="row mb-3">
         <div class="col-md-3">
             <?php include "status_acuerdos_sesion/boton.php" ?>
         </div>
         <div class="col-md-3">
             <?php include "status_sesiones/boton.php" ?>
         </div>
+    </div>
+    <div class="row mb-3">
         <div class="col-md-3">
             <?php include "tipo_actores/boton.php" ?>
         </div>
         <div class="col-md-3">
             <?php include "tipo_consejos/boton.php" ?>
         </div>
-    </div>
-    <div class="row mb-3">
         <div class="col-md-3">
             <?php include "usuarios/boton.php" ?>
         </div>
