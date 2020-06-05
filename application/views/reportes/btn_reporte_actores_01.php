3bb9ba3937a1b2d5c85b864754f5cce96789f732
Carlos Escutia <carlosescutia@gmail.com>
2020-06-05 11:40:39 -0500

    Se corrige url de imagenes de reportes.

--- application/views/reportes/btn_reporte_actores_01.php
+++ application/views/reportes/btn_reporte_actores_01.php
@@ -7,7 +7,7 @@
 					<p>Base de datos de Actores con datos básicos y de participación.</p>
 				</div>
 				<div class="col-md-4">
-					<img src="http://192.168.56.101/participacion/img/reporte_actores_01.png" class="img-fluid img-thumbnail p-0 m-0">
+					<img src="<?=base_url();?>img/reporte_actores_01.png" class="img-fluid img-thumbnail p-0 m-0">
 				</div>
 			</div>
 		</div>
--- application/views/reportes/btn_reporte_consejos_01.php
+++ application/views/reportes/btn_reporte_consejos_01.php
@@ -7,7 +7,7 @@
 					<p>Base de datos de Consejos con datos básicos, sesiones e integrantes.</p>
 				</div>
 				<div class="col-md-4">
-					<img src="http://192.168.56.101/participacion/img/reporte_consejos_01.png" class="img-fluid img-thumbnail p-0 m-0">
+					<img src="<?=base_url();?>img/reporte_consejos_01.png" class="img-fluid img-thumbnail p-0 m-0">
 				</div>
 			</div>
 		</div>
