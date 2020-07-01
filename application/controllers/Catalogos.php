2184cbfe69f6da957bf889ebefee8084409479cc
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-07-01 11:47:59 -0500

    Se elimina funcion ambitos que no se utiliza.

--- application/controllers/Catalogos.php
+++ application/controllers/Catalogos.php
@@ -44,27 +44,4 @@ class Catalogos extends CI_Controller {
         }
     }
 
-    public function ambitos()
-    {
-        if ($this->session->userdata('logueado')) {
-            $data['usuario_clave'] = $this->session->userdata('clave');
-            $data['usuario_nombre'] = $this->session->userdata('nombre');
-            $dependencia = $this->session->userdata('dependencia');
-            $data['usuario_dependencia'] = $dependencia;
-            $area = $this->session->userdata('area');
-            $data['usuario_area'] = $area;
-            $cve_rol = $this->session->userdata('cve_rol');
-            $data['cve_rol'] = $cve_rol;
-            if ($cve_rol != 'adm') {
-                redirect('inicio');
-            }
-
-            $this->load->view('templates/header', $data);
-            $this->load->view('catalogos/ambitos', $data);
-            $this->load->view('templates/footer');
-        } else {
-            redirect('inicio/iniciar_sesion');
-        }
-    }
-
 }
