a30745b017f2d9899cf92dfc32de48e6415ebfd5
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-08-25 11:30:31 -0500

    Se completa catálogo de Planteamientos originales.

---
+++ application/controllers/Planteamientos.php
@@ -0,0 +1,114 @@
+<?php
+class Planteamientos extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('planteamientos_model');
+    }
+
+    public function lista()
+    {
+        if ($this->session->userdata('logueado')) {
+            $data['usuario_clave'] = $this->session->userdata('clave');
+            $data['usuario_nombre'] = $this->session->userdata('nombre');
+            $dependencia = $this->session->userdata('dependencia');
+            $data['usuario_dependencia'] = $dependencia;
+            $area = $this->session->userdata('area');
+            $data['usuario_area'] = $area;
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_rol'] = $cve_rol;
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $data['planteamientos'] = $this->planteamientos_model->get_planteamientos();
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/planteamientos/lista', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+    public function detalle($cve_planteamiento)
+    {
+        if ($this->session->userdata('logueado')) {
+            $data['usuario_clave'] = $this->session->userdata('clave');
+            $data['usuario_nombre'] = $this->session->userdata('nombre');
+            $dependencia = $this->session->userdata('dependencia');
+            $data['usuario_dependencia'] = $dependencia;
+            $area = $this->session->userdata('area');
+            $data['usuario_area'] = $area;
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_rol'] = $cve_rol;
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $data['planteamientos'] = $this->planteamientos_model->get_planteamiento($cve_planteamiento);
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/planteamientos/detalle', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+    public function nuevo()
+    {
+        if ($this->session->userdata('logueado')) {
+            $data['usuario_clave'] = $this->session->userdata('clave');
+            $data['usuario_nombre'] = $this->session->userdata('nombre');
+            $dependencia = $this->session->userdata('dependencia');
+            $data['usuario_dependencia'] = $dependencia;
+            $area = $this->session->userdata('area');
+            $data['usuario_area'] = $area;
+            $cve_rol = $this->session->userdata('cve_rol');
+            $data['cve_rol'] = $cve_rol;
+            if ($cve_rol != 'adm') {
+                redirect('inicio');
+            }
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('catalogos/planteamientos/nuevo', $data);
+            $this->load->view('templates/footer');
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+    public function guardar($cve_planteamiento=null)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            $planteamientos = $this->input->post();
+            if ($planteamientos) {
+                $data = array(
+                    'nom_planteamiento' => empty($planteamientos['nom_planteamiento']) ? null : $planteamientos['nom_planteamiento']
+                );
+                $this->planteamientos_model->guardar($data, $cve_planteamiento);
+            }
+            redirect('planteamientos/lista');
+
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+    public function eliminar($cve_planteamiento)
+    {
+        if ($this->session->userdata('logueado')) {
+
+            $this->planteamientos_model->eliminar($cve_planteamiento);
+            redirect('planteamientos/lista');
+
+        } else {
+            redirect('inicio/iniciar_sesion');
+        }
+    }
+
+}
