da88f5831bfd111797f375ff092545bb8071e824
Carlos Escutia <cescutia@guanajuato.gob.mx>
2020-09-25 09:49:57 -0500

    Se simplifican accesos al sistema por rol en reportes.

--- application/views/reportes/lista.php
+++ application/views/reportes/lista.php
@@ -6,18 +6,18 @@
         <div class="row">
             <div class="col-md-6">
                 <h3>Listados</h3>
-                <?php if (in_array('04001', $accesos_sistema_rol)) include "btn_reporte_actores_01.php"; ?>
-                <?php if (in_array('04002', $accesos_sistema_rol)) include "btn_listado_actores_02.php" ?>
-                <?php if (in_array('04003', $accesos_sistema_rol)) include "btn_reporte_consejos_01.php"; ?>
-                <?php if (in_array('04004', $accesos_sistema_rol)) include "btn_listado_consejos_02.php"; ?>
-                <?php if (in_array('04005', $accesos_sistema_rol)) include "btn_reporte_proyectos_01.php"; ?>
+                <?php if (in_array('04100', $accesos_sistema_rol)) include "btn_reporte_actores_01.php"; ?>
+                <?php if (in_array('04101', $accesos_sistema_rol)) include "btn_listado_actores_02.php" ?>
+                <?php if (in_array('04100', $accesos_sistema_rol)) include "btn_reporte_consejos_01.php"; ?>
+                <?php if (in_array('04100', $accesos_sistema_rol)) include "btn_listado_consejos_02.php"; ?>
+                <?php if (in_array('04100', $accesos_sistema_rol)) include "btn_reporte_proyectos_01.php"; ?>
             </div>
             <div class="col-md-6">
                 <h3>Estadísticos</h3>
-                <?php if (in_array('04006', $accesos_sistema_rol)) include "btn_reporte_totales_acuerdos.php"; ?>
-                <?php if (in_array('04007', $accesos_sistema_rol)) include "btn_reporte_totales_proyectos.php"; ?>
-                <?php if (in_array('04008', $accesos_sistema_rol)) include "btn_estadistico_consejos_01.php"; ?>
-                <?php if (in_array('04009', $accesos_sistema_rol)) include "btn_estadistico_actores_01.php" ?>
+                <?php if (in_array('04200', $accesos_sistema_rol)) include "btn_reporte_totales_acuerdos.php"; ?>
+                <?php if (in_array('04200', $accesos_sistema_rol)) include "btn_reporte_totales_proyectos.php"; ?>
+                <?php if (in_array('04200', $accesos_sistema_rol)) include "btn_estadistico_consejos_01.php"; ?>
+                <?php if (in_array('04200', $accesos_sistema_rol)) include "btn_estadistico_actores_01.php" ?>
             </div>
         </div>
     </div>
