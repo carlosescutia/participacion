792793d794be3538e37cf8e7bb08475a86766ff1
Carlos Escutia <carlosescutia@gmail.com>
2021-10-07 12:26:32 -0500

    Se modifica base_url de testing a tstparticipacion.guanajuato.gob.mx.

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pubparticipacion.guanajuato.gob.mx/';
+$config['base_url'] = 'http://tstparticipacion.guanajuato.gob.mx/';
 
 /*
 |--------------------------------------------------------------------------
