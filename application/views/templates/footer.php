2bff8b114c483c1cf46fa8e21f64bec25a314440
Carlos Escutia <cescutia@guanajuato.gob.mx>
2022-05-05 11:49:20 -0500

    Se reduce tamaño del pie de página y del encabezado.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,31 +1,16 @@
         </div>
-        <footer class="masfoot d-print-none ml-sm-auto px-4">
-            <div class="row">
-                <div class="col-md-1">
-                </div>
-                <div class="col-md-2">
-                    <img class="img-fluid mb-3" src="<?=base_url()?>img/gto_iplaneg.png" style="max-height: 50px; padding-top:10px;">
-                </div>
-                <div class="col-md-3">
-                    <p class="section_title">ATENCIÓN CIUDADANA</p>
-                    <p><i class="fa fa-phone"></i> 472 103 7700</p>
-                    <p><i class="fa fa-envelope"></i> iplaneg@guanajuato.gob.mx</p>
-                </div>
-                <div class="col-md-1">
-                    <img class="img-fluid mb-3" src="<?=base_url()?>img/logo-gl.svg" style="max-height: 100px; padding-top:10px; ">
-                </div>
-                <div class="col-md-3">
-                    <p class="section_title">NUESTRA SEDE</p>
-                    <a href="https://www.google.com/maps/place/Instituto+de+Planeaci%C3%B3n,+Estad%C3%ADstica+y+Geograf%C3%ADa+del+Estado+de+Guanajuato+-+IPLANEG/@21.0079503,-101.5081362,17z/data=!3m1!4b1!4m5!3m4!1s0x842b99ebb0c8e155:0xd74201edd922fff6!8m2!3d21.0079336!4d-101.5059451" target="_blank">
-                        <p><i class="fa fa-map-marker"></i> Blvd Mineral de Valenciana 351</p>
-                        <p>Puerto interior de Gto. C.P. 36275</p>
-                        <p>Silao de la Victoria, Gto.</p>
-                    </a>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <a href="https://transparencia.guanajuato.gob.mx/conocenos/aviso_privacidad.php" target="_blank"><p>Aviso legal © 2022 Gobierno del Estado de Guanajuato</p></a>
+        <div class="footer_margin">
+        </div>
+        <footer class="footer masfoot d-flex d-print-none justify-content-center">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="mr-5 ml-5">
+                        <img class="img-fluid mb-3" src="<?=base_url()?>img/gto_iplaneg.png" style="max-height: 40px">
+                    </div>
+                    <div>
+                        <p><i class="fa fa-phone"></i> 472 103 7700 <i class="fa fa-envelope"></i> iplaneg@guanajuato.gob.mx</p>
+                        <p>© 2022 Gobierno del Estado de Guanajuato</p></a>
+                    </div>
                 </div>
             </div>
         </footer>
