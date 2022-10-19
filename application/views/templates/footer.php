43d25a3afd7b782ef7ac15aa8673a61c97cd3d72
Carlos Escutia <carlosescutia@gmail.com>
2022-10-19 16:36:44 -0500

    Se reduce tamaño del pie de página.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,13 +3,9 @@
         </div>
         <footer class="footer masfoot d-flex d-print-none justify-content-center">
             <div class="col-md-6">
-                <div class="row">
-                    <div class="mr-5 ml-5">
-                        <img class="img-fluid mb-3" src="<?=base_url()?>img/gto_iplaneg.png" style="max-height: 40px">
-                    </div>
+                <div class="row mb-2">
                     <div>
-                        <p><i class="fa fa-phone"></i> 472 103 7700 <i class="fa fa-envelope"></i> iplaneg@guanajuato.gob.mx</p>
-                        <p>© 2022 Gobierno del Estado de Guanajuato</p></a>
+                        <p><i class="fa fa-phone"></i> 472 103 7700 &nbsp; · &nbsp; <i class="fa fa-envelope"></i> iplaneg@guanajuato.gob.mx &nbsp; · &nbsp; © 2023 Gobierno del Estado de Guanajuato</p></a>
                     </div>
                 </div>
             </div>
